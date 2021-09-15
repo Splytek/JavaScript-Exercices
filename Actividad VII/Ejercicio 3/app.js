35d4f85f1e5d05f7fa00b759b7732333a007e64a
GHU1GA <external.HugoEmmanuel.GonzalezGomez@mx.bosch.com>
2021-09-15 10:52:30 -0500

    v1.915

--- Actividad VII/Ejercicio 3/app.js
+++ Actividad VII/Ejercicio 3/app.js
@@ -1,2 +1,14 @@
-/* 3.- crear una función que reciba dos números, entero 1 y entero 2, el entero 1 va a ser el multiplicando y el numero dos el 
-multiplicador y al final se imprimirá la tabla de multiplicar. */
+/* 3.- crear una función que reciba dos números, entero 1 y entero 2, el entero 1 va a ser el multiplicando y el numero 
+dos el multiplicador y al final se imprimirá la tabla de multiplicar. */
+
+const tablaMult = (entero1, entero2) => {
+  for(var i=0; i<10; i+=2){
+    r = entero1 *= entero2;
+    document.write(`${entero1} x ${i} = ${r} </br>`);
+  }
+}
+
+var num1 = parseInt(prompt("Escribe le multiplicando: "));
+var num2 = parseInt(prompt("Escribe le multiplicador: "));
+
+tablaMult(num1, num2);
---
+++ Actividad VIII/Ejercicio 1/app.js
@@ -0,0 +1,10 @@
+/* 1.	Imprimir todos los dígitos del 0 al 9, utilizando una repetición. */
+var numeros = [0,1,2,3,4,5,6,7,8,9];
+
+const rNumeros = (numeros) => {
+  for(var i=0; i<numeros.length; i++){
+    console.log(numeros[i]);
+  }
+}
+
+rNumeros(numeros);
